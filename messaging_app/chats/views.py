fc7905fa3c5c5fc851011e966b1f785ba7122609
Peter Mburu <peter.mburu@kimetrica.com>
2025-07-20 11:58:38 +0300

    add viewsets

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,5 @@
-from rest_framework import permissions, viewsets
+from rest_framework import filters, permissions, status, viewsets
+from rest_framework.response import Response
 
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
@@ -12,6 +13,8 @@ class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["created_at"]
 
 
 class MessageViewSet(viewsets.ModelViewSet):
@@ -22,3 +25,14 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["sent_at"]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
