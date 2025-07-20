fa9b8bda93fa7a8255dfdd71b5b538dc0d1ac2da
Peter Mburu <peter.mburu@kimetrica.com>
2025-07-20 12:02:05 +0300

    add urls

---
+++ messaging_app/chats/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import ConversationViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r"conversations", ConversationViewSet)
+router.register(r"messages", MessageViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
