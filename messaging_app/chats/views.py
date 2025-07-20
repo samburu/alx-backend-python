253e261d8a2e9fc5ce4f1829612c6f413c548fa3
Peter Mburu <peter.mburu@kimetrica.com>
2025-07-20 11:55:44 +0300

    add viewsets

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for listing and creating Conversations.
+    """
+
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for listing and creating Messages in Conversations.
+    """
+
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
