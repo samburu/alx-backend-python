61b5a6a8b301a70294da860a3754e9ff35f1bfa5
Peter Mburu <peter.mburu@kimetrica.com>
2025-07-26 20:20:13 +0300

    add pagination

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -137,7 +137,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
         "chats.auth.CustomTokenAuthentication",
     ],
-    "DEFAULT_PAGINATION_CLASS": "chats.pagination.MessagePagination",
+    "DEFAULT_PAGINATION_CLASS": "chats.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend"
