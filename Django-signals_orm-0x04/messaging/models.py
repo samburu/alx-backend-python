98f6c3c3312a6d1bfe422d792e18a81d7b364cd1
Peter Mburu <peter.mburu@kimetrica.com>
2025-07-30 14:42:42 +0300

    add signal

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -14,6 +14,7 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+    is_edited = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Message from {self.sender} to {self.receiver}"
@@ -27,3 +28,18 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"Notification for {self.user} - Message ID {self.message.id}"
+
+
+class MessageHistory(models.Model):
+    """
+    Model to store message history for users.
+    This can be used to track read/unread messages.
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    is_read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History for {self.user} - Message ID {self.message.id}"
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 from .models import Message, Notification
@@ -11,3 +11,24 @@ def notify_user_on_message(sender, instance, created, **kwargs):
     """
     if created:
         Notification.objects.create(user=instance.receiver, message=instance)
+
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    """
+    Signal to track message history before saving.
+    This can be used to update read status or other attributes.
+    """
+    if instance.pk:
+        # If the message already exists, we can update its history
+        try:
+            previous_message = Message.objects.get(pk=instance.pk)
+            # If the read status has changed, we can log this change
+            MessageHistory.objects.create(
+                user=instance.receiver,
+                message=instance,
+                is_read=instance.is_read,
+            )
+            instance.is_edited = True  # Mark as edited if it exists
+        except Message.DoesNotExist:
+            pass
